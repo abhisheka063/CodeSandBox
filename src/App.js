c13ac179c93193dbe20b4568b297c8c3937d1476
Abhishek yadav <40734291+abhisheka063@users.noreply.github.com>
2022-03-30 15:06:55 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -18,9 +18,8 @@ export default function App() {
 
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-      <ToolTip position={styles.left} />
+      <h1>React Classic</h1>
+      <ToolTip position={styles.top} />
     </div>
   );
 }
