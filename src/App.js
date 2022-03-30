9d629d5ed6c4664f608d99ec84596b2adbb9670b
Abhishek yadav <40734291+abhisheka063@users.noreply.github.com>
2022-03-30 17:35:04 +0530

    Moved styles to Toolkit component and Added switch

--- src/App.js
+++ src/App.js
@@ -2,28 +2,12 @@ import "./styles.css";
 import ToolTip from "./Tooltip";
 
 export default function App() {
-  // Creating position styles and passing them as props
-  let styles = {
-    left: {
-      right: "100%",
-      top: "-25px"
-    },
-    top: {
-      bottom: "50px"
-    },
-    right: {
-      left: "100%",
-      top: "-25px"
-    },
-    bottom: {
-      bottom: "0px"
-    }
-  };
+  // Passing positions as props
 
   return (
     <div className="App">
       <h1>React Classic</h1>
-      <ToolTip position={styles.bottom} />
+      <ToolTip position="top" />
     </div>
   );
 }
--- src/Tooltip.js
+++ src/Tooltip.js
@@ -1,9 +1,49 @@
 import "./styles.css";
 function ToolTip(props) {
+  let setPosition = () => {
+    let position;
+    // Creating styles for different positions
+    let styles = {
+      left: {
+        right: "100%",
+        top: "-25px"
+      },
+      top: {
+        bottom: "50px"
+      },
+      right: {
+        left: "100%",
+        top: "-25px"
+      },
+      bottom: {
+        bottom: "0px"
+      }
+    };
+    // Setting positions according to recived props
+    switch (props.position) {
+      case "top":
+        position = styles.top;
+        break;
+      case "bottom":
+        position = styles.bottom;
+        break;
+      case "left":
+        position = styles.left;
+        break;
+      case "right":
+        position = styles.right;
+        break;
+      default:
+        break;
+    }
+    // Returning positions
+    return position;
+  };
+
   return (
     <div className="tooltip">
       Hover over me
-      <span className="tooltiptext" style={props.position}>
+      <span className="tooltiptext" style={setPosition()}>
         Hi! I am Tooltip
       </span>
     </div>
